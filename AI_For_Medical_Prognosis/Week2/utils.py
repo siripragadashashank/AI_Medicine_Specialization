cdd880cf0b3589b2e4a6cabde00f83ad036c2955
Shashank Siripragada <siripragada.s@northeastern.edu>
2022-03-24 11:50:56 -0400

    moved DataLoader to utils

--- AI_For_Medical_Prognosis/Week2/utils.py
+++ AI_For_Medical_Prognosis/Week2/utils.py
@@ -4,32 +4,31 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-__location__ = os.path.realpath(
-    os.path.join(os.getcwd(), os.path.dirname(__file__))
-)
 
 class DataLoader:
 
 	def __init__(self, threshold):
-
-		super(DataLoader, self).__init__()
+		#super(DataLoader, self).__init__()
 		self.threshold = threshold
-		self.__location__ = os.path.realpath(
-							os.path.join(os.getcwd(), 
-							os.path.dirname(__file__))
-					)
 
 	def cindex(self, y_true, scores):
 		return lifelines.utils.concordance_index(y_true, scores)
 
 	def nhanesi(self, display=False):
-		X = pd.read_csv(os.path.join(self.__location__, 'NHANESI_subset_X.csv'))
-		y = pd.read_csv(os.path.join(self.__location__, 'NHANESI_subset_y.csv'))["y"]
+		__location__ = os.path.realpath(
+					os.path.join(os.getcwd(), 
+					os.path.dirname(__file__))
+			)
+		X = pd.read_csv(os.path.join(__location__, 'NHANESI_subset_X.csv'))
+		y = pd.read_csv(os.path.join(__location__, 'NHANESI_subset_y.csv'))["y"]
 		if display:
 			X_display = X.copy()
 			X_display['Sex'] = ['Male' if v==1 else 'Female' for v in X['Sex']]
 			return X_display, np.array(y)
 		return X, np.array(y)
+
+	def prob_drop(self, age):
+		return 1 - (np.exp(0.25 * age - 5) / (1 + np.exp(0.25 * age - 5)))
 
 	def load_data(self):
 		X, y = self.nhanesi()
@@ -44,18 +43,19 @@ class DataLoader:
 		X = df.drop(['time', 'death'], axis='columns')
 		y = df.time < self.threshold
 
+
 		X_dev, X_test, y_dev, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
 		feature_y = 'Systolic BP'
 		frac = 0.7
 
-		drop_rows = X_dev.sample(frac=frac, replace=False,
-								weights=[prob_drop(X_dev.loc[i, 'Age']) for i in
-								X_dev.index], random_state=10)
+		drop_rows = X_dev.sample(
+								frac=frac, 
+								replace=False,
+								weights=[self.prob_drop(X_dev.loc[i, 'Age']) for i in X_dev.index], 
+								random_state=10
+								)
 		drop_rows.loc[:, feature_y] = None
 		drop_y = y_dev[drop_rows.index]
 		X_dev.loc[drop_rows.index, feature_y] = None
 
 		return X_dev, X_test, y_dev, y_test
-
-	def prob_drop(self, age):
-		return 1 - (np.exp(0.25 * age - 5) / (1 + np.exp(0.25 * age - 5)))
