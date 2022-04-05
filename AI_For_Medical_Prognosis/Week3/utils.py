b74dd64113bfaf82f6749921a8f118ca18148241
Shashank Siripragada <siripragada.s@northeastern.edu>
2022-04-05 01:55:19 -0400

    adding utils

--- AI_For_Medical_Prognosis/Week3/utils.py
+++ AI_For_Medical_Prognosis/Week3/utils.py
@@ -1,9 +1,8 @@
 from lifelines.datasets import load_lymphoma
 
-class DataLoader:
 
-	def load_data(self):
-		df = load_lymphoma()
-		df[:, 'Event'] = df.Censor
-		df.drop(['Censor'], axis=1)
-		return df
+def load_data():
+    df = load_lymphoma()
+    df.loc[:, 'Event'] = df.Censor
+    df = df.drop(['Censor'], axis=1)
+    return df
